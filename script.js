5faa5eef65c4a3893b6fc67318ca31b3bc04bae7
Rob8624 <robert_melen@yahoo.co.uk>
2024-08-23 01:32:59 +0100

    header animations updated

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-function toggleMenu() {
+function fadeIn() {
+    const menu = document.querySelector('.menu');
+    let opacity = 0
+    menu.style.display = 'block';
+    menu.style.opacity = opacity;
 
+
+    let interval = setInterval(function() {
+        if (opacity < 1){
+            opacity = opacity + 0.1
+            menu.style.opacity = opacity
+        } else {
+            clearInterval(interval)
+        }
+    }, 100) 
+}
+
+function toggleMenu() {
     const headerMenu = document.querySelector('.header-bottom');
+    const hamburger = document.querySelector('.hamburger'); 
     headerMenu.classList.toggle('active');
-    const hamburger = document.querySelector('.hamburger');
-    if (headerMenu.classList.contains('active')) {
-        hamburger.innerText = '\\'; // Change to ||| when active
-    } else {
-        hamburger.innerText = '/'; // Change to /// when not active
+    hamburger.classList.toggle('open');
+
+    if (hamburger.classList.contains('open')){
+        fadeIn()
     }
 }
+
+
+
